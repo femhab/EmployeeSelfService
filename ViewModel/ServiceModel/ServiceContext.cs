6600fe7f43e8481aeb956cc866a64aec145c72ac
Habeebullah Babatunde <babatundehabeebullah@gmail.com>
2020-11-19 00:33:27 +0100

    [Babatunde] Context added to ServiceModel plus removalmof Unneccessary Files

---
+++ ViewModel/ServiceModel/ServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ViewModel.ServiceModel
+{
+    public class ServiceContext : DbContext
+    {
+        public ServiceContext(DbContextOptions<ServiceContext> options): base(options) { }
+
+        public DbSet<HRDept> HRDept { get; set; }
+        public DbSet<HRGrade> HRGrade { get; set; }
+        public DbSet<HRUsers> HRUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
